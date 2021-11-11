be7f6f7b4ef38210960a614bc8cb1eb8f54c716d
Akauã Santos da Costa <akauasantosdacosta@MacBook-Pro-de-Akaua.local>
2021-11-11 16:18:45 +0100

    adjust dependencies python

--- flux-sample-app-2/cd_flux/lambda_cd.py
+++ flux-sample-app-2/cd_flux/lambda_cd.py
@@ -1,8 +1,8 @@
 import json
 import os
-# import .dependency.boto3
-from dependency.aws_xray_sdk.core import xray_recorder
-from dependency.aws_xray_sdk.core import patch_all  # Patch all supported libraries for X-Ray - More info: https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python-patching.html
+import boto3
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all  # Patch all supported libraries for X-Ray - More info: https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python-patching.html
 patch_all()
 
 
