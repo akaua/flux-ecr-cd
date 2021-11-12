f771800a78cabfb607b07d17b78e5624c287e22b
Akauã Santos da Costa <akauasantosdacosta@mbp-de-akaua.kpn>
2021-11-12 16:16:55 +0100

    adjust python env

--- flux-sample-app-2/cd_flux/lambda_cd.py
+++ flux-sample-app-2/cd_flux/lambda_cd.py
@@ -8,5 +8,30 @@ patch_all()
 
 def lambda_handler(event, context):
     print(event)
+    runtime_region = os.environ['AWS_REGION']
+    print(runtime_region)
+    github_token = os.environ['GITHUB_TOKEN']
+    print(github_token)
+    ecr_repository = os.environ['ECR_REPOSITORY']
+    print(ecr_repository)
 
     return event
+
+# def discover_delete_images(region):
+
+
+
+
+# response = client.batch_get_image(
+#     registryId='string',
+#     repositoryName='string',
+#     imageIds=[
+#         {
+#             'imageDigest': 'string',
+#             'imageTag': 'string'
+#         },
+#     ],
+#     acceptedMediaTypes=[
+#         'string',
+#     ]
+# )
