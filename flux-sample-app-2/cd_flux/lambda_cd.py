7a04831b5306f5d896c9ce35b25ba24bfecfd544
Akauã Santos da Costa <akauasantosdacosta@mbp-de-akaua.kpn>
2021-11-15 16:21:46 +0100

    adjust to comment git lib

--- flux-sample-app-2/cd_flux/lambda_cd.py
+++ flux-sample-app-2/cd_flux/lambda_cd.py
@@ -1,7 +1,7 @@
 import json
 import os
 import boto3
-from git import Repo
+# from git import Repo
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all  # Patch all supported libraries for X-Ray - More info: https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python-patching.html
 patch_all()
